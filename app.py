c9e8524de311ce7a6bc7b39191bc55b74f082e7f
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-13 06:10:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # %%writefile app.py
 import streamlit as st
-import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import celassification_report, accuracy_score, precision_score, recall_score 
 from sklearn import neighbors, tree, svm
