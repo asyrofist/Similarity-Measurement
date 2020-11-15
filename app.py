fe6c4ad93818abbcbe5c690f552d8a096b1ac2d2
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-15 11:15:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if index0 is not None:
        st.subheader('Cosine Visualisation') 
        st.line_chart(hasil_cosine, use_container_width=True) 
         
-       c = alt.Chart(cos).mark_circle().encode(x=id_requirement[0], y=id_requirement[1], size=id_requirement[2], color=id_requirement[2], tooltip=id_requirement)
+       c = alt.Chart(cos).mark_circle().encode(, tooltip=id_requirement)
        st.altair_chart(c, use_container_width=True)
        
     
