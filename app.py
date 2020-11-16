65ce42c8889cf89e659bd490d431efd928f3f569
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-17 05:43:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,9 +62,6 @@ if index0 is not None:
        # feature collection
        st.subheader('Feature  parameters')
        desc_pmi = df_jumlahpmi.describe()
-       opsi_pmi = st.multiselect('What Feature do you remove?',['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'],['count'])
-       desc_pmi = desc_pmi.drop(opsi_pmi, axis=0)
-       desc_pmi = desc_pmi.T
        st.write(desc_pmi)
         
        fig, ax = plt.subplots(figsize=(10,10))
@@ -88,9 +85,6 @@ if index0 is not None:
        # feature collection
        st.subheader('Feature  parameters')
        desc_vsm = df_vsm.describe()
-       opsi_vsm = st.multiselect('What Feature do you remove?',['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'],['count'])
-       desc_vsm = desc_vsm.drop(opsi_vsm, axis=0)
-       desc_vsm = desc_svd.T
        st.write(desc_vsm)
         
        fig, ax = plt.subplots(figsize=(10,10))
@@ -118,10 +112,6 @@ if index0 is not None:
        # feature collection
        st.subheader('Feature  parameters')
        desc_svd = df_svd.describe()
-       opsi_svd = st.multiselect('What Feature do you remove?',['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'],['count'])
-       desc_svd = desc_svd.drop(opsi_svd, axis=0)
-       desc_svd = desc_svd.T
-       hasil = desc_svd
        st.write(desc_svd)
         
        fig, ax = plt.subplots(figsize=(10,10))
