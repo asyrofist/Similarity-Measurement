8e1b2adad6f6b6eeba5e6c117908f7b8cbdc5a5d
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-19 09:13:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,16 @@ def traintestANN(x, y, rasio,  input1, input2, input3,
     # Adam optimizer with learning rate of 0.001
     optimizer = Adam(lr= learning_rate)
     model.compile(optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    hasil_model = model.summary()
+    st.write('Neural Network Model Summary: ')
+    st.write(hasli_model)
+
+    # Train the model
+    model.fit(train_x, train_y, verbose= verbose_value, batch_size= batch_value, epochs= epoch_value)  
+    results = model.evaluate(test_x, test_y)
+    st.write('Evaluasi ANN: ')
+    st.write(results)
+
 
 st.write("""
 # Requirement Dependency Measurements
@@ -713,13 +723,6 @@ if index0 is not None:
                          aktivasi1, aktivasi2, aktivasi3, 
                          layer1, layer2, layer3, learning_rate, 
                          verbose_value, batch_value, epoch_value)
-            st.write('Neural Network Model Summary: ')
-            hasil_model = model.summary()
-            st.write(hasli_model)
-
-            # Train the model
-            model.fit(train_x, train_y, verbose= verbose_value, batch_size= batch_value, epochs= epoch_value)  
-            results = model.evaluate(test_x, test_y)
             results_ = confusion_matrix(text_x, test_y)
             # visual
             fig, ax = plt.subplots()
